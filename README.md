7b899e2c57b27529d26b7ce93540b132b88aee23
seveniruby <seveniruby@gmail.com>
2021-01-05 22:23:07 +0800

    layout demo

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # frontend_lagou4
+[霍格沃兹测试学院拉勾名企直推营第四期演练](https://ceshiren.com/c/9-category/65-category/65)
+
+## 霍格沃兹测试学院
+- [测试人论坛](https://ceshiren.com)
+- [霍格沃兹测试学院](https://testing-studio.com)
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-navigation-drawer app clipped permanent>
+      <!-- -->
+      <Nav />
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary" dark clipped-left>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -37,21 +38,42 @@
       </v-btn>
     </v-app-bar>
 
-    <router-view></router-view>
     <v-main>
-      <HelloWorld/>
+      <!-- <v-container fluid>
+        <v-row>
+        <v-col cols="3">
+        <v-navigation-drawer>
+        
+        <Nav/>
+       
+       
+        </v-navigation-drawer>
+       </v-col>
+       <v-col cols="9">
+        <router-view></router-view>
+        </v-col>
+        <HelloWorld/>
+        </v-row>
+      </v-container> -->
+
+      <v-container fluid>
+        <router-view></router-view>
+        <!-- <HelloWorld/> -->
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import Nav from "./components/Nav";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Nav,
   },
 
   data: () => ({
